9f0e8dd0b6dc4925e2e33a9ca7f8850a06bfdc02
Kene Lin <kenelin@gmail.com>
2019-08-21 23:04:16 +0800

    Add NetworkInterface

---
+++ source/main/java/com/htc/vita/core/net/NetworkInterface.java
@@ -0,0 +1,61 @@
+package com.htc.vita.core.net;
+
+import com.htc.vita.core.log.Logger;
+
+import java.net.InetAddress;
+import java.net.InterfaceAddress;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Map;
+
+public class NetworkInterface {
+    public static Map<InetAddress, InetAddress> getLocalIPv4AddressesWithBroadcastAddress()
+    {
+        Map<InetAddress, InetAddress> result = new HashMap<InetAddress, InetAddress>();
+        try {
+            Enumeration<java.net.NetworkInterface> networkInterfaces = java.net.NetworkInterface.getNetworkInterfaces();
+            while (networkInterfaces.hasMoreElements()) {
+                java.net.NetworkInterface networkInterface = networkInterfaces.nextElement();
+                for (InterfaceAddress interfaceAddress : networkInterface.getInterfaceAddresses()) {
+                    if (interfaceAddress == null) {
+                        continue;
+                    }
+
+                    InetAddress inetAddress = interfaceAddress.getAddress();
+                    if (inetAddress == null) {
+                        continue;
+                    }
+
+                    InetAddress broadcastAddress = interfaceAddress.getBroadcast();
+                    if (broadcastAddress == null || "0.0.0.0".equals(broadcastAddress.getHostAddress())) {
+                        continue;
+                    }
+
+                    result.put(inetAddress, broadcastAddress);
+                }
+            }
+        } catch (Exception e) {
+            Logger.getInstance(NetworkInterface.class.getSimpleName()).error("Can not get local IP addresses with broadcast addresses, error: " + e.getMessage());
+        }
+
+        return result;
+    }
+
+    public static boolean isNetworkAvailable()
+    {
+        try {
+            Enumeration<java.net.NetworkInterface> networkInterfaces = java.net.NetworkInterface.getNetworkInterfaces();
+            while (networkInterfaces.hasMoreElements()) {
+                java.net.NetworkInterface networkInterface = networkInterfaces.nextElement();
+
+                if (!networkInterface.isLoopback() && networkInterface.isUp()) {
+                    return true;
+                }
+            }
+        } catch (Exception e) {
+            Logger.getInstance(NetworkInterface.class.getSimpleName()).error("Can not detect if network is available, error: " + e.getMessage());
+        }
+
+        return false;
+    }
+}
