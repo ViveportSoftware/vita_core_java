ae7df8a2cac4d4aeb2a123cab74e40747af97c92
Kene Lin <kenelin@gmail.com>
2020-07-05 16:17:40 +0800

    Add fromHexString() and toHexString() in Convert

--- source/main/java/com/htc/vita/core/util/Convert.java
+++ source/main/java/com/htc/vita/core/util/Convert.java
@@ -3,6 +3,33 @@ package com.htc.vita.core.util;
 import com.htc.vita.core.log.Logger;
 
 public class Convert {
+    public static byte[] fromHexString(String content) {
+        if (StringUtils.isNullOrEmpty(content)) {
+            return null;
+        }
+
+        int size = content.length();
+        byte[] result = new byte[size / 2];
+        for (int i = 0; i < size; i += 2) {
+            int upperByte = Character.digit(content.charAt(i), 16);
+            int lowerByte = Character.digit(content.charAt(i + 1), 16);
+            result[i / 2] = (byte) (upperByte * 16 + lowerByte);
+        }
+        return result;
+    }
+
+    public static String toHexString(byte[] bytes) {
+        if (bytes == null) {
+            return "";
+        }
+
+        StringBuilder buffer = new StringBuilder(bytes.length * 2);
+        for (byte b: bytes) {
+            buffer.append(String.format("%02x", b));
+        }
+        return buffer.toString();
+    }
+
     public static int toInt32(String content) {
         return toInt32(content, 0);
     }
