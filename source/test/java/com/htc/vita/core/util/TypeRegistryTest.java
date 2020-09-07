50adf69d279d488159632a024b53bcbbe92b5791
Kene Lin <kenelin@gmail.com>
2020-09-07 14:44:49 +0800

    Add TypeRegistry

---
+++ source/test/java/com/htc/vita/core/util/TypeRegistryTest.java
@@ -0,0 +1,53 @@
+package com.htc.vita.core.util;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TypeRegistryTest {
+    @Test
+    public void default_0_registerDefault() {
+        TypeRegistry.registerDefault(
+                BaseClass.class,
+                SubClass1.class
+        );
+    }
+
+    @Test
+    public void default_1_register() {
+        TypeRegistry.registerDefault(
+                BaseClass.class,
+                SubClass1.class
+        );
+        TypeRegistry.register(
+                BaseClass.class,
+                SubClass2.class
+        );
+    }
+
+    @Test
+    public void default_2_getInstance() {
+        TypeRegistry.registerDefault(
+                BaseClass.class,
+                SubClass1.class
+        );
+        TypeRegistry.register(
+                BaseClass.class,
+                SubClass2.class
+        );
+        BaseClass obj = TypeRegistry.getInstance(BaseClass.class);
+        Assert.assertFalse(obj instanceof SubClass1);
+        Assert.assertTrue(obj instanceof SubClass2);
+    }
+
+    public static abstract class BaseClass
+    {
+    }
+
+    public static class SubClass1 extends BaseClass
+    {
+    }
+
+    public static class SubClass2 extends BaseClass
+    {
+    }
+}
