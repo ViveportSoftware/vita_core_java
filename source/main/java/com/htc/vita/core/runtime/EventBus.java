8319d320b6b11237255c8b3e73694fb19f486123
Kene Lin <kenelin@gmail.com>
2020-09-07 16:10:45 +0800

    Refine EventBus with TypeRegistry

--- source/main/java/com/htc/vita/core/runtime/EventBus.java
+++ source/main/java/com/htc/vita/core/runtime/EventBus.java
@@ -1,96 +1,32 @@
 package com.htc.vita.core.runtime;
 
 import com.htc.vita.core.log.Logger;
-
-import java.lang.reflect.Constructor;
-import java.util.HashMap;
-import java.util.Map;
+import com.htc.vita.core.util.TypeRegistry;
 
 public abstract class EventBus {
-    private static final Map<String, EventBus> sInstances = new HashMap<String, EventBus>();
-    private static final Object sInstancesLock = new Object();
-
-    private static Class<? extends EventBus> sDefaultClass = DefaultEventBus.class;
+    static {
+        TypeRegistry.registerDefault(
+                EventBus.class,
+                DefaultEventBus.class
+        );
+    }
 
     public static <T extends EventBus> void register(Class<T> clazz) {
-        if (sDefaultClass == clazz) {
-            return;
-        }
-
-        sDefaultClass = clazz;
-        Logger.getInstance(EventBus.class.getSimpleName()).info(String.format(
-                "Registered default %s type to %s%n",
-                EventBus.class.getSimpleName(),
-                sDefaultClass.getName()
-        ));
+        TypeRegistry.register(
+                EventBus.class,
+                clazz
+        );
     }
 
     public static EventBus getInstance() {
-        return getInstance(sDefaultClass);
+        return TypeRegistry.getInstance(EventBus.class);
     }
 
     public static <T extends EventBus> EventBus getInstance(Class<T> clazz) {
-        EventBus instance;
-        try {
-            instance = doGetInstance(clazz);
-        } catch (Exception e) {
-            Logger.getInstance(EventBus.class.getSimpleName()).fatal(String.format(
-                    "Instance initialization error: %s",
-                    e
-            ));
-            Logger.getInstance(EventBus.class.getSimpleName()).info(String.format(
-                    "Initializing %s...",
-                    DefaultEventBus.class.getName()
-            ));
-            instance = new DefaultEventBus();
-        }
-        return instance;
-    }
-
-    private static <T extends EventBus> EventBus doGetInstance(Class<T> clazz) {
-        if (clazz == null) {
-            throw new IllegalArgumentException(String.format(
-                    "Invalid argument to get %s instance",
-                    EventBus.class.getSimpleName()
-            ));
-        }
-
-        String key = String.format(
-                "%s_",
-                clazz.getName()
+        return TypeRegistry.getInstance(
+                EventBus.class,
+                clazz
         );
-        EventBus instance = null;
-        if (sInstances.containsKey(key)) {
-            instance = sInstances.get(key);
-        }
-        if (instance == null) {
-            Logger.getInstance(EventBus.class.getSimpleName()).info(String.format(
-                    "Initializing %s...",
-                    key
-            ));
-            try {
-                Constructor<T> constructor = clazz.getConstructor();
-                instance = constructor.newInstance();
-            } catch (Exception e) {
-                // Skip
-            }
-        }
-        if (instance == null) {
-            Logger.getInstance(EventBus.class.getSimpleName()).info(String.format(
-                    "Initializing %s...",
-                    DefaultEventBus.class.getName()
-            ));
-            instance = new DefaultEventBus();
-        }
-        synchronized (sInstancesLock) {
-            if (!sInstances.containsKey(key)) {
-                sInstances.put(
-                        key,
-                        instance
-                );
-            }
-        }
-        return instance;
     }
 
     public <T extends IEventData> boolean registerListener(
