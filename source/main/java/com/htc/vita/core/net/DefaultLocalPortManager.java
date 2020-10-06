0125a4ad1d9b07f451a22418f56d087212ea535d
Kene Lin <kenelin@gmail.com>
2020-10-06 11:40:22 +0800

    Return last port if it is still unused

--- source/main/java/com/htc/vita/core/net/DefaultLocalPortManager.java
+++ source/main/java/com/htc/vita/core/net/DefaultLocalPortManager.java
@@ -1,15 +1,31 @@
 package com.htc.vita.core.net;
 
+import com.htc.vita.core.log.Logger;
+import com.htc.vita.core.util.StringUtils;
+
 import java.io.IOException;
 import java.net.ServerSocket;
 
 public class DefaultLocalPortManager extends LocalPortManager {
-    @Override
-    protected int onGetRandomUnusedPort() throws IOException {
+    private static int lastLocalPort = 0;
+
+    private static int doGetUnusedPort(int preferredPort) throws IOException {
+        int realPreferredPort = preferredPort;
+        if (realPreferredPort < 0 || realPreferredPort > 65535) {
+            Logger.getInstance(DefaultLocalPortManager.class.getSimpleName()).warn(StringUtils.rootLocaleFormat(
+                    "preferred port number %d is invalid.",
+                    realPreferredPort
+            ));
+            realPreferredPort = 0;
+        }
         ServerSocket serverSocket = null;
         try {
-            serverSocket = new ServerSocket(0);
+            serverSocket = new ServerSocket(realPreferredPort);
             return serverSocket.getLocalPort();
+        } catch (IOException e) {
+            if (realPreferredPort == 0) {
+                throw e;
+            }
         } finally {
             if (serverSocket != null) {
                 try {
@@ -19,5 +35,15 @@ public class DefaultLocalPortManager extends LocalPortManager {
                 }
             }
         }
+        return 0;
+    }
+
+    @Override
+    protected int onGetRandomUnusedPort() throws IOException {
+        lastLocalPort = doGetUnusedPort(lastLocalPort);
+        if (lastLocalPort == 0) {
+            lastLocalPort = doGetUnusedPort(lastLocalPort);
+        }
+        return lastLocalPort;
     }
 }
--- source/test/java/com/htc/vita/core/net/LocalPortManagerTest.java
+++ source/test/java/com/htc/vita/core/net/LocalPortManagerTest.java
@@ -8,11 +8,14 @@ import org.junit.Test;
 public class LocalPortManagerTest {
     @Test
     public void default_0_getRandomUnusedPort() {
-        int unusedPort = LocalPortManager.getInstance().getRandomUnusedPort();
-        Assert.assertFalse(unusedPort < 0);
+        int unusedPort0 = LocalPortManager.getInstance().getRandomUnusedPort();
+        Assert.assertFalse(unusedPort0 < 0);
         Logger.getInstance(LocalPortManagerTest.class.getSimpleName()).info(StringUtils.rootLocaleFormat(
                 "unused port: %d",
-                unusedPort
+                unusedPort0
         ));
+        int unusedPort1 = LocalPortManager.getInstance().getRandomUnusedPort();
+        Assert.assertFalse(unusedPort1 < 0);
+        Assert.assertEquals(unusedPort0, unusedPort1);
     }
 }
